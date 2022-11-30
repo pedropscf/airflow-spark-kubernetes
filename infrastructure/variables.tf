6377f64a708b6fd8cb639d018665fd6d3fe8b434
pedropscf <xurreia@LAPTOP-T149JGR0.localdomain>
2022-11-29 21:33:45 -0300

    Atualizando terraform iac

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -8,6 +8,16 @@ variable "subnet_id_2" {
   default = "subnet-0cb00729297568a24"
 }
 
+variable "subnet_id_3" {
+  type = string
+  default = "subnet-0bb38455f8fd16795"
+}
+
+variable "vpc_id" {
+  type = string
+  default = "	vpc-07dd8fc1853ce4c9f"
+}
+
 variable "eks_name" {
   type = string
   default = "pedropscf-eks"
--- infrastructure/workers.tf
+++ infrastructure/workers.tf
@@ -18,12 +18,15 @@ resource "aws_eks_node_group" "worker-node-group" {
   cluster_name  = aws_eks_cluster.devopsthehardway-eks.name
   node_group_name = var.eks_name
   node_role_arn  = aws_iam_role.workernodes.arn
-  subnet_ids   = [var.subnet_id_1, var.subnet_id_2]
-  instance_types = ["t3.xlarge"]
- 
+  #vpc_ids = var.vpc_id
+  subnet_ids   = [var.subnet_id_1, var.subnet_id_2, var.subnet_id_3]
+  ami_type       = "AL2_x86_64" # AL2_x86_64, AL2_x86_64_GPU, AL2_ARM_64, CUSTOM
+  capacity_type  = "SPOT"  # ON_DEMAND, SPOT
+  disk_size      = 20
+  instance_types = ["m5.large"]
   scaling_config {
    desired_size = 1
-   max_size   = 1
+   max_size   = 3
    min_size   = 1
   }
  
